3cd2bb893a227c9e3e1f0dcd6c98ec199b357e50
lishuxiang <lishuxiang@cug.edu.cn>
2020-01-01 17:34:30 +0800

    Show time cost messagebox

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,67 +1,83 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow), n(8)
-{
-    srand((unsigned int)time(nullptr));
-    ui->setupUi(this);
-    db = new DrawBoard(n);
-    setCentralWidget(db);
-    auto action_look_back = ui->menuBar->addAction("回溯法");
-    connect(action_look_back, SIGNAL(triggered()), SLOT(look_back()));
-    auto action_hill_climbing = ui->menuBar->addAction("重启爬山法");
-    connect(action_hill_climbing, SIGNAL(triggered()), SLOT(hill_climbing()));
-    auto action_simulated_anneal = ui->menuBar->addAction("模拟退火算法");
-    connect(action_simulated_anneal, SIGNAL(triggered()), SLOT(simulated_anneal()));
-    auto action_genetic = ui->menuBar->addAction("遗传算法");
-    connect(action_genetic, SIGNAL(triggered()), SLOT(genetic()));
-    auto action_set_n = ui->menuBar->addAction("参数设置");
-    connect(action_set_n, SIGNAL(triggered()), SLOT(set_arg()));
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent), ui(new Ui::MainWindow), n(8) {
+  srand((unsigned int)time(nullptr));
+  ui->setupUi(this);
+  db = new DrawBoard(n);
+  setCentralWidget(db);
+  auto action_look_back = ui->menuBar->addAction("回溯法");
+  connect(action_look_back, SIGNAL(triggered()), SLOT(look_back()));
+  auto action_hill_climbing = ui->menuBar->addAction("重启爬山法");
+  connect(action_hill_climbing, SIGNAL(triggered()), SLOT(hill_climbing()));
+  auto action_simulated_anneal = ui->menuBar->addAction("模拟退火算法");
+  connect(action_simulated_anneal, SIGNAL(triggered()),
+          SLOT(simulated_anneal()));
+  auto action_genetic = ui->menuBar->addAction("遗传算法");
+  connect(action_genetic, SIGNAL(triggered()), SLOT(genetic()));
+  auto action_set_n = ui->menuBar->addAction("参数设置");
+  connect(action_set_n, SIGNAL(triggered()), SLOT(set_arg()));
 }
 
 void MainWindow::look_back() {
-    LookBack lb(n);
-    lb.look_back_queen(0);
-    db->set_data(lb.solutions[1]);
+  LookBack lb(n);
+  clock_t start_t = clock();
+  lb.look_back_queen(0);
+  clock_t end_t = clock();
+  db->set_data(lb.solutions[1]);
+  db->update();
+  float time_cost = (end_t - start_t) * 1000 * 1000 / CLOCKS_PER_SEC;
+  QString time_str = QString::number(time_cost);
+  QMessageBox::information(this, "Time cost", time_str + "μs");
+}
+void MainWindow::hill_climbing() {
+  clock_t start_t = clock();
+  auto res = ::hill_climbing(n, 600000);
+  clock_t end_t = clock();
+  if (res) {
+    db->set_data(res.get());
     db->update();
-}
-void MainWindow::hill_climbing(){
-  auto res = ::hill_climbing(n, 100000);
-  if (res) {
-      db->set_data(res.get());
-      db->update();
   } else {
-      QMessageBox::information(this, "Fail", "未能找到解");
+    QMessageBox::information(this, "Fail", "未能找到解");
   }
+  float time_cost = (end_t - start_t) * 1000 * 1000 / CLOCKS_PER_SEC;
+  QString time_str = QString::number(time_cost);
+  QMessageBox::information(this, "Time cost", time_str + "μs");
 }
-void MainWindow::simulated_anneal(){
-  auto res = ::simulated_anneal(n, 999999);
+void MainWindow::simulated_anneal() {
+  clock_t start_t = clock();
+  auto res = ::simulated_anneal(n, 9999999);
+  clock_t end_t = clock();
   if (res) {
-      db->set_data(res.get());
-      db->update();
+    db->set_data(res.get());
+    db->update();
   } else {
-      QMessageBox::information(this, "Fail", "未能找到解");
+    QMessageBox::information(this, "Fail", "未能找到解");
   }
+  float time_cost = (end_t - start_t) * 1000 * 1000 / CLOCKS_PER_SEC;
+  QString time_str = QString::number(time_cost);
+  QMessageBox::information(this, "Time cost", time_str + "μs");
 }
-void MainWindow::genetic(){
+void MainWindow::genetic() {
   Nature nature(indi_count, n, max_gen, mutate_rate);
+  clock_t start_t = clock();
   auto res = nature.evolve_population(nullptr);
+  clock_t end_t = clock();
   if (res) {
-      db->set_data(res.get());
-      db->update();
+    db->set_data(res.get());
+    db->update();
   } else {
-      QMessageBox::information(this, "Fail", "未能找到解");
+    QMessageBox::information(this, "Fail", "未能找到解");
   }
+  float time_cost = (end_t - start_t) * 1000 * 1000 / CLOCKS_PER_SEC;
+  QString time_str = QString::number(time_cost);
+  QMessageBox::information(this, "Time cost", time_str + "μs");
 }
-MainWindow::~MainWindow()
-{
-    delete ui;
-}
+MainWindow::~MainWindow() { delete ui; }
 void MainWindow::set_arg() {
-    Dialog dlg(n);
-    dlg.exec();
-    n = dlg.n;
-    db->set_n(n);
+  Dialog dlg(n);
+  dlg.exec();
+  n = dlg.n;
+  db->set_n(n);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,38 +1,39 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
-#include <QMessageBox>
 #include "dialog.h"
 #include "drawboard.h"
 #include "genetic.h"
 #include "hill_climbing.h"
 #include "look_back.h"
 #include "simulated_anneal.h"
+#include <QMainWindow>
+#include <QMessageBox>
+#include <time.h>
 
 namespace Ui {
 class MainWindow;
 }
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+  Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = nullptr);
-    int n;
-    DrawBoard *db;
-    int indi_count = 40, max_gen = 10000;
-    float mutate_rate = 0.08;
-    ~MainWindow();
+  explicit MainWindow(QWidget *parent = nullptr);
+  int n;
+  DrawBoard *db;
+  int indi_count = 40, max_gen = 50000;
+  float mutate_rate = 0.2;
+  ~MainWindow();
 private slots:
-    void set_arg();
-    void look_back();
-    void hill_climbing();
-    void simulated_anneal();
-    void genetic();
+  void set_arg();
+  void look_back();
+  void hill_climbing();
+  void simulated_anneal();
+  void genetic();
+
 private:
-    Ui::MainWindow *ui;
+  Ui::MainWindow *ui;
 };
 
 #endif // MAINWINDOW_H
